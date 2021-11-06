678f32e7f9c004e78d63c2134ba38a31e58c0865
I.Rud <yarudvan@gmail.com>
2021-11-06 20:11:55 +0200

    not working

---
+++ if_else.py
@@ -0,0 +1,14 @@
+def funcionforif (a,b,c):
+    if a > b:
+     print (c)
+    if a < b:
+          print ("Земля не круглая,как думал, ты раньше")
+    if a == b:
+        print ("Но она слегка приплюснута по полюсам!")
+
+funcionforif (5, 7, 1)
+funcionforif (2, 1, 3)
+funcionforif (4, 7, 6)
+
+
+
